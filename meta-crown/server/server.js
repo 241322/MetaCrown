1b651f0fc0f97369c5305f2ee2ad59de84c7bb89
Xander Poalses <241322@virtualwindow.co.za>
2025-09-25 03:24:53 +0200

    trying to do an otp authentication
    SendGrid bugging out

--- meta-crown/server/server.js
+++ meta-crown/server/server.js
@@ -1,9 +1,30 @@
 import express from 'express';
 import cors from 'cors';
 import mysql from 'mysql';
+import bodyParser from 'body-parser';
+import twilio from 'twilio';
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Twilio Credentials
+const accountSID = 'ACa82ed20293b580ecc9c1232a42b6fe44';
+const authToken = '63e4ad0312e6185c617736dfbb6294c7';
+const twilioClient = twilio(accountSID, authToken);
+const otpStore = {};
+
+// Endpoint to send OTP
+app.post('/send-otp', (req, res) => {
+  const { phoneNumber } = req.body;
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  otpStore[phoneNumber] = otp;
+
+  Client.messages
+    .create({
+      body: `Your OTP is ${otp}`,
+      from: 
 
 const db = mysql.createConnection({
   host: 'localhost',
